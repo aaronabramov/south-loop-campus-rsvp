9c5e0e431d7fde9f25e96e4d3466081cfcfd67b1
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-08-12 19:40:22 -0500

    Redirect to main website

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SouthLoopCampusRsvp::Application.routes.draw do
+  get "*path", to: redirect("http://citychurchchicago.com/")
+  root to: redirect("http://citychurchchicago.com/")
+
   root to: "application#home"
   resources :rsvps, only: [:create, :index]
 
   post :notify, to: "logs#notify"
+
   resources :logs, only: :index
 end
